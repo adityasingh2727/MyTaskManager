cfc1ed8ed254fc4958492c6734219d46d0107ec9
Aditya Singh <adityaaa.singh27@gmail.com>
2025-05-19 22:22:16 +0530

    CreateTask tab updated

--- frontend/Task-Manager/src/components/Inputs/SelectUsers.jsx
+++ frontend/Task-Manager/src/components/Inputs/SelectUsers.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import axiosInstance from '../../utils/axiosInstance';
 import API_PATHS from '../../utils/apiPaths';
 import { LuUsers } from 'react-icons/lu';
+import Modal from '../Modal';
+import AvatarGroup from '../AvatarGroup';
 
 const SelectUsers = ({selectedUsers , setSelectedUsers}) => {
     
@@ -56,6 +58,54 @@ const SelectUsers = ({selectedUsers , setSelectedUsers}) => {
             <LuUsers className='text-sm' /> Add Member
         </button>
       )}
+
+        {selectedUsersAvatars.length > 0 && (
+            <div className='cursor-pointer' onClick={() => setIsModalOpen(true)}> 
+             <AvatarGroup avatars={selectedUsersAvatars} maxVisible={3} />
+             </div>
+        )}
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title= "Select Users"
+        >
+            <div className='space-y h-[60vh] overflow-y-auto'>
+                {allUsers.map((user) => (
+                    <div 
+                      key={user._id}
+                      className='flex items-center gap-4 p-3 border-b border-gray-200'
+                    >
+                        <img 
+                            src={user.profileImageUrl}
+                            alt={user.name}
+                            className='w-10 h-10 rounded-full'
+                        />
+                        <div className='flex-1'>
+                            <p className='font-medium text-gray-800 dark:text-white'>
+                                {user.name}
+                            </p>
+                            <p className='text-[13px] text-gray-500'>{user.email}</p>
+                        </div>  
+                        <input 
+                            type='checkbox'
+                            checked={tempSelectedUsers.includes(user._id)}
+                            onChange={() => toggleUserSelection(user._id)}
+                            className='w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none'
+                        />
+                    </div>
+                ))}
+            </div>
+
+            <div className='flex justify-end gap-4 pt-4 '>
+                <button className='card-btn mb-8' onClick={() => setIsModalOpen(false)}> 
+                    CANCEL
+                </button>
+                <button className='card-btn-fill mb-8 mr-4' onClick={hadnleAssign}>
+                    DONE
+                </button>
+            </div>
+        </Modal>
     </div>
   )
 }
---
+++ frontend/Task-Manager/src/components/Inputs/TodoListInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HiMiniPlus, HiOutlineTrash } from "react-icons/hi2";
+
+const TodoListInput = () => {
+  return (
+    <div>
+      TodoListInput
+    </div>
+  )
+}
+
+export default TodoListInput
